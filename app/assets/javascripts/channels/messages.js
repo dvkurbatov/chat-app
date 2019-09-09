634011dd9e279871848796af23979742bb38cb33
Денис Курбатов <denis.kurbatov@saltpepper.ru>
2019-09-09 23:04:39 +0300

    Fix error: new message draw only one channel

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,12 +1,19 @@
-App.messages = App.cable.subscriptions.create('MessagesChannel', {
+App.messages = App.cable.subscriptions.create(
+  {
+    channel: 'MessagesChannel'
+  },
+  {
   received: function(data) {
-    $("#empty_chat").remove();
-    if ($('#messages').children().length > 10){
-      $('#messages').children().first().remove();
+    if ($('#messages').data('channel-id') == data.channel.id){
+      $("#empty_chat").remove();
+      if ($('#messages').children().length > 10){
+        $('#messages').children().first().remove();
+      }
+      $("#messages").append(this.renderMessage(data));
     }
-    $("#messages").append(this.renderMessage(data));
   },
   renderMessage: function(data) {
     return '<div><span>' + data.nickname + ': ' + data.message + '</span></div>';
   }
+
 });
