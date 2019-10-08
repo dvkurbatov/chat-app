34a897b08737a24095bbc02763c14c064c573830
Денис Курбатов <denis.kurbatov@saltpepper.ru>
2019-10-08 22:31:36 +0300

    no message

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def current_user
-    if user_id = cookies.signed[:user_id]
+    if user_id = cookies.encrypted[:user_id]
       @current_user ||= User.find_by(id: user_id)
     else
       redirect_to new_session_path
